9b8c8d24a757eabd92298a1ba373eebf699e45f6
Michał Wysocki <01181184@pw.edu.pl>
2025-10-28 17:56:29 +0100

    :-)

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,25 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource_ptr;
+public:
+    ResourceManager() : resource_ptr(new Resource()) {}
+    double get() { return resource_ptr->get(); }
+    ~ResourceManager() { delete resource_ptr; }
+    ResourceManager(const ResourceManager& other) = delete;
+    ResourceManager& operator=(const ResourceManager& other) = delete;
+    ResourceManager(ResourceManager&& other) noexcept : resource_ptr(other.resource_ptr)
+    {
+        other.resource_ptr = nullptr;
+    }
+    ResourceManager& operator=(ResourceManager&& other) noexcept
+    {
+        if (this != &other) {
+            resource_ptr = other.resource_ptr;
+
+            other.resource_ptr = nullptr;
+        }
+        return *this;
+    }
 };
