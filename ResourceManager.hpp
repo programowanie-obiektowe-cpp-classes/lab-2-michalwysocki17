687487397d5f3fbf589205d5ee67aa5e62b3c750
Michał Wysocki <01181184@pw.edu.pl>
2025-10-28 18:10:31 +0100

    :-(

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -6,22 +6,47 @@ class ResourceManager
 {
 private:
     Resource* resource_ptr;
+
+    void cleanup()
+    {
+        if (resource_ptr != nullptr) {
+            delete resource_ptr;
+            resource_ptr = nullptr;
+        }
+    }
+
 public:
     ResourceManager() : resource_ptr(new Resource()) {}
+
     double get() { return resource_ptr->get(); }
-    ~ResourceManager() { delete resource_ptr; }
-    ResourceManager(const ResourceManager& other) : resource_ptr(new Resource(*other.resource_ptr)) {};
+
+    ~ResourceManager() { cleanup(); }
+
+    ResourceManager(const ResourceManager& other) : resource_ptr(new Resource(*other.resource_ptr))
+    {}
+
     ResourceManager& operator=(const ResourceManager& other)
     {
         if (this != &other) {
             Resource* temp_resource = new Resource(*other.resource_ptr);
+
+            cleanup();
+
             resource_ptr = temp_resource;
         }
         return *this;
     }
+
+    ResourceManager(ResourceManager&& other) noexcept : resource_ptr(other.resource_ptr)
+    {
+        other.resource_ptr = nullptr;
+    }
+
     ResourceManager& operator=(ResourceManager&& other) noexcept
     {
         if (this != &other) {
+            cleanup();
+
             resource_ptr = other.resource_ptr;
 
             other.resource_ptr = nullptr;
